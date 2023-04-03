d0c43de54c269c733a26c6d9bc08b0207614c4b7
Nikolai Timonin <nikki.timonin@yandex.ru>
2023-04-03 11:29:06 +0600

    Add example project

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Nikolai Timonin on 03.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
